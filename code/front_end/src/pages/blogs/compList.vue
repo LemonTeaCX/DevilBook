bbb65a2a6f3336b762efc2d0e7ae9c7a70473621
lemon <1406428663@qq.com>
2019-03-14 17:35:00 +0800

    Create compList.vue

---
+++ code/front_end/src/pages/blogs/compList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <list-view 
+      :title="'组件列表'"
+      :tip="'这里是本人日常工作与学习开发过的一些前端组件，可点击查看详情。'"
+      :list="comps"
+      :detailPath="'/blogs/compDetail/'"></list-view>
+  </div>
+</template>
+
+<script>
+import { getComps } from '../../api';
+import ListView from '../../components/listView';
+export default {
+  name: 'CompList',
+  components: { ListView },
+  data() {
+    return {
+      comps: []
+    }
+  },
+  mounted() {
+    this.getCompList();
+  },
+  methods: {
+    async getCompList() {
+      let result = await getComps();
+      this.comps = result.data;
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
