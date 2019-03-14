2586e3a51bf0df5b74b86a1fa0e69dfd83993cc6
lemon <1406428663@qq.com>
2019-03-14 17:33:48 +0800

    Update blogList.vue

--- code/front_end/src/pages/blogs/blogList.vue
+++ code/front_end/src/pages/blogs/blogList.vue
@@ -1,25 +1,19 @@
 <template>
-  <div class="blog-list">
-    <h2>blog-list</h2>
-    <el-alert
-      title="blogtips"
-      type="info"
-      show-icon
-      :closable="false">
-    </el-alert>
-    <div class="blogs-list">
-      <div class="blogs-item" v-for="(blog, index) in blogs" :key="blog.id">
-        <el-button type="text" @click="blogDetail(blog.id)">{{`${index+1}. ${blog.name}`}}</el-button>
-        <i class="el-icon-edit source-url" title="查看原文" @click="jumpSource(blog.url)"></i>
-      </div>
-    </div>
+  <div>
+    <list-view 
+      :title="'博客列表'"
+      :tip="'这里是本人日常工作与学习积累的一些web开发知识，可点击查看详情，也可以点击博客标题右边按钮查看原文'"
+      :list="blogs"
+      :detailPath="'/blogs/blogDetail/'"></list-view>
   </div>
 </template>
 
 <script>
 import { getBlogs } from '../../api';
+import ListView from '../../components/listView';
 export default {
   name: 'BlogList',
+  components: { ListView },
   data() {
     return {
       blogs: []
@@ -32,38 +26,10 @@ export default {
     async getBlogList() {
       let result = await getBlogs();
       this.blogs = result.data;
-    },
-    blogDetail(id = 1) {
-      this.$router.push({
-        path: '/blogs/blogDetail/',
-        query: { id }
-      });
-    },
-    jumpSource(url = '') {
-      window.top.open && window.top.open(url);
     }
   }
 }
 </script>
 
 <style lang="less">
-.blog-list {
-  margin: 30px;
-  padding: 20px;
-  border: 1px solid #ddd;
-  box-shadow: 0 0 20px #ccc;
-}
-.blogs-item {
-  .source-url {
-    display: none;
-    margin-left: 10px;
-    cursor: pointer;
-  }
-  &:hover {
-    .source-url {
-      display: inline-block;
-    }
-  }
-}
-
 </style>
