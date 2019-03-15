909f8ec96039affb2996f8937b5747a02b52e9bc
lemon <1406428663@qq.com>
2019-03-15 17:31:28 +0800

    Create inputText.vue

---
+++ code/front_end/src/components/inputText.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-text">
+    <div v-if="!editing" @click="startEdit">{{value}}</div>
+    <el-input v-else v-model="value" size="small" @blur="endEdit"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputText',
+  props: {
+    text: [String, Number, Boolean],
+  },
+  data() {
+    return {
+      value: '',
+      editing: false
+    }
+  },
+  mounted() {
+    this.value = this.text;
+  },
+  methods: {
+    startEdit() {
+      this.editing = true;
+    },
+    endEdit() {
+      this.editing = false;
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.input-text {
+  cursor: pointer;
+}
+</style>
